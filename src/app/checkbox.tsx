9706a716aa8776ff4a73d5e6757437cca65f05e4
Denis Rojcyk <denis@rojcyk.com>
2020-08-09 19:11:17 +0200

    Adding checkbox component

---
+++ src/app/checkbox.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Checkbox = ({ 
+    label,
+    isSelected,
+    onCheckboxChange
+}: {
+    label: string,
+    isSelected: boolean,
+    onCheckboxChange: any
+}) => (
+  <div>
+    <label>
+      <input
+        type="checkbox"
+        name={label}
+        checked={isSelected}
+        onChange={onCheckboxChange}
+      />
+      {label}
+    </label>
+  </div>
+);
+
+export default Checkbox;
