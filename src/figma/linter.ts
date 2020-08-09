a22624914a72d8b582eb6e5cee8a6dc16c8ab525
Denis Rojcyk <denis@rojcyk.com>
2020-08-09 21:24:42 +0200

    Linter is now looping over the user selection, displays a toast message if nothing is selected

--- src/figma/linter.ts
+++ src/figma/linter.ts
@@ -2,16 +2,24 @@ import io from "figmaio/code"
 import { APP_LINT } from "../constants/events"
 import { ComparingEngine } from './searchEngine'
 import { importStyles } from './importStyles'
-// import asyncForEach from "../helpers/asyncForEach"
+import asyncForEach from "../helpers/asyncForEach"
+import { handleNode } from "./nodeHandler"
   
 export const linter = async (styles: any[]) => {
-  io.on(APP_LINT, async (data) => {
+  io.on(APP_LINT, async (settings) => {
     await importStyles()
 
+    const selection = figma.currentPage.selection
     const search = new ComparingEngine(styles)
 
     console.log('Linting ...')
 
-    search.show()
+    if (selection.length === 0) {
+      figma.notify('You need to select something to lint 👅')
+    } else {
+      asyncForEach(selection as any[], (frame: any) => {
+        handleNode(frame, settings)
+      })
+    }
   })
 }
