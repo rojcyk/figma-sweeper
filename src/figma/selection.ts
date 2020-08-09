da8273e91634d21d7cb87d9464106b3d6c1f2f9b
Denis Rojcyk <denis@rojcyk.com>
2020-08-09 20:39:34 +0200

    Removing unused code

--- src/figma/selection.ts
+++
@@ -1,8 +0,0 @@
-import { handleNode } from './nodeHandler'
-
-export const selection = (settings: any) => {
-  for (const node of figma.currentPage.selection) {
-    // console.log(node)
-    handleNode(node, settings)
-  }
-}
