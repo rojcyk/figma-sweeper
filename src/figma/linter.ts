eb451434a71bafcf2d426e882543427434246a11
Denis Rojcyk <denis@rojcyk.com>
2020-08-10 22:43:50 +0200

    Testing basic color index functionality

--- src/figma/linter.ts
+++ src/figma/linter.ts
@@ -12,9 +12,48 @@ export const linter = async (styles: any[]) => {
     const selection = figma.currentPage.selection
     const colorIndex = new ColorIndex(styles)
 
+    console.log('Imported styles ...')
+    console.log(styles)
+
+    console.log('Processed colors ...')
+    colorIndex.processed()
+
+    console.log(colorIndex.findColor({
+      b: 0.7686274647712708,
+      g: 0.7686274647712708,
+      r: 0.7686274647712708
+    }))
+
+    console.log(colorIndex.findColor({
+      b: 0.7686274647712709,
+      g: 0.7686274647712708,
+      r: 0.7686274647712708
+    }))
+
+    const red = {
+      b: 0.16333329677581787,
+      g: 0.16333329677581787,
+      r: 0.8166666626930237,
+    }
+
+    const darkRed = {
+      b: 0.06444445252418518,
+      g: 0.06444445252418518,
+      r: 0.4833333194255829
+    }
+
+    const gray = {
+      b: 0.7686274647712708,
+      g: 0.7686274647712708,
+      r: 0.7686274647712708
+    }
+
+    console.log(colorIndex.compare(red,darkRed))
+    console.log(colorIndex.compare(red,gray))
+
     console.log('Linting ...')
 
-    if (selection.length === 0) {
+    if (selection?.length === 0) {
       figma.notify('You need to select something to lint 👅')
     } else {
       asyncForEach(selection as any[], (frame: any) => {
