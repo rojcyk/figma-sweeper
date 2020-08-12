6fd37a96b159581823e9f622bd760d563ea4253f
Denis Rojcyk <denis@rojcyk.com>
2020-08-12 23:22:21 +0200

    Commented out test functions

--- src/figma/linter.ts
+++ src/figma/linter.ts
@@ -5,51 +5,39 @@ import { importStyles } from './importStyles'
 import asyncForEach from "../helpers/asyncForEach"
 import { handleNode } from "./nodeHandler"
   
-export const linter = async (styles: any[]) => {
+export const linter = async () => {
   io.on(APP_LINT, async (settings) => {
-    await importStyles()
+    const imported = await importStyles()
 
     const selection = figma.currentPage.selection
-    const colorIndex = new ColorIndex(styles)
+    const colorIndex = new ColorIndex(imported)
 
-    console.log('Imported styles ...')
-    console.log(styles)
+    // console.log('Imported styles ...')
+    // console.log(imported)
 
-    console.log('Processed colors ...')
-    colorIndex.processed()
+    // console.log('Processed colors ...')
+    // colorIndex.processed()
 
-    console.log(colorIndex.findColor({
-      b: 0.7686274647712708,
-      g: 0.7686274647712708,
-      r: 0.7686274647712708
-    }))
+    // const red = {
+    //   b: 0.16333329677581787,
+    //   g: 0.16333329677581787,
+    //   r: 0.8166666626930237,
+    // }
 
-    console.log(colorIndex.findColor({
-      b: 0.7686274647712709,
-      g: 0.7686274647712708,
-      r: 0.7686274647712708
-    }))
+    // const darkRed = {
+    //   b: 0.06444445252418518,
+    //   g: 0.06444445252418518,
+    //   r: 0.4833333194255829
+    // }
 
-    const red = {
-      b: 0.16333329677581787,
-      g: 0.16333329677581787,
-      r: 0.8166666626930237,
-    }
+    // const gray = {
+    //   b: 0.7686274647712708,
+    //   g: 0.7686274647712708,
+    //   r: 0.7686274647712708
+    // }
 
-    const darkRed = {
-      b: 0.06444445252418518,
-      g: 0.06444445252418518,
-      r: 0.4833333194255829
-    }
-
-    const gray = {
-      b: 0.7686274647712708,
-      g: 0.7686274647712708,
-      r: 0.7686274647712708
-    }
-
-    console.log(colorIndex.compare(red,darkRed))
-    console.log(colorIndex.compare(red,gray))
+    // console.log(colorIndex.compare(red,darkRed))
+    // console.log(colorIndex.compare(red,gray))
 
     console.log('Linting ...')
 
