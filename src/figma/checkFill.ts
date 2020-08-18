efbec69aea2be879ba728bcd693bb0b4001bc2dd
Denis Rojcyk <denis@rojcyk.com>
2020-08-18 21:44:24 +0200

    Improving fill detection

--- src/figma/checkFill.ts
+++ src/figma/checkFill.ts
@@ -1,24 +1,33 @@
-import { traverse } from './traverse'
+import { ColorIndex } from './colorIndex'
 
-const adjustColor = (color: any) => {
-  // console.log(color)
-  // const d = Math.sqrt((r2-r1)^2 + (g2-g1)^2 + (b2-b1)^2)
+export const isSettable = (node: any) => {
+  let visibleLayers = 0
+  let valid = true
+
+  node.fills.forEach((paintStyle: any) => {
+    if (paintStyle.visible) {
+      visibleLayers++
+
+      if (paintStyle.type !== 'SOLID') {
+        valid = false
+      }
+    } 
+  })
+
+  if (visibleLayers > 1) valid = false
+  
+  return valid
 }
 
-const colorCheck = (color: any) => {  
-  switch (color.type) {
-    case 'SOLID':
-      adjustColor(color.color)
-      break
-    case 'GRADIENT_LINEAR':
-    case 'GRADIENT_RADIAL':
-    case 'GRADIENT_ANGULAR':
-    case 'GRADIENT_DIAMOND':
-      for (const item of color.gradientStops) adjustColor(item.color)
-      break
+export const checkFills = (node: any, colorIndex: ColorIndex, settings: any) => {
+  const isValid = isSettable(node)
+
+  if (isValid) {
+    const color = node.fills[0].color
+    const foundColor = colorIndex.findColor(color)
+
+    if (foundColor) {
+      node.fillStyleId = foundColor.id
+    }
   }
-}
-
-export const checkFills = (fills: any) => {
-  if (fills) traverse(fills, colorCheck)
 }
