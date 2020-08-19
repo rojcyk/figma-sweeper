18b7eb0452218d19430ea49b2a7da37ff0360436
Denis Rojcyk <denis@rojcyk.com>
2020-08-19 22:56:14 +0200

    Added working switch for strokes

--- src/app/setting.tsx
+++ src/app/setting.tsx
@@ -5,15 +5,18 @@ import { APP_LINT } from "../constants/events"
 
 export const SettingsForm = () => {
   const [fills, setFills] = React.useState(true)
+  const [strokes, setStrokes] = React.useState(true)
   
   const switchFills = () => setFills(!fills)
+  const switchStrokes = () => setStrokes(!strokes)
 
   return (
     <form onSubmit={(e) => {
       e.preventDefault()
 
       io.send(APP_LINT, {
-        fills
+        fills,
+        strokes
       })
     }}>
 
@@ -39,13 +42,13 @@ export const SettingsForm = () => {
         fontSize: '12px'
       }}>Make sure to <a href="https://help.figma.com/hc/en-us/articles/360038006754-Version-History#:~:text=Figma%20will%20periodically%20save%20versions,even%20more%20space%20to%20explore." target="blank">make a new version</a> prior to reconnecting. It isn't required, but it might save you some headaches. The plugin will check and overwrite the following properties.</p>
 
-      <Checkbox label='Overwrite Fills' isSelected={fills}  onCheckboxChange={switchFills} /><br />
+      <Checkbox label='Overwrite Fills' isSelected={fills}  onCheckboxChange={switchFills} />
+      <Checkbox label='Overwrite Strokes' isSelected={strokes}  onCheckboxChange={switchStrokes} />
 
-      <hr /><br />
+      <br /><hr /><br />
 
       <b>Not implemented</b><br /><br />
 
-      <Checkbox label='Overwrite stroke' isSelected={false}  onCheckboxChange={() => { }} />
       <Checkbox label='Overwrite already set styles' isSelected={false}  onCheckboxChange={() => { }} />
       <Checkbox label='Ignore opacity' isSelected={false}  onCheckboxChange={() => { }} /><br /><br />
     </form>
