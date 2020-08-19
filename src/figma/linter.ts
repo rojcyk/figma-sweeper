1fc354c546f86f9eaa13cce55e43925268465a6f
Denis Rojcyk <denis@rojcyk.com>
2020-08-19 20:58:20 +0200

    Removed old comments

--- src/figma/linter.ts
+++ src/figma/linter.ts
@@ -9,37 +9,12 @@ export const linter = async () => {
   io.on(APP_LINT, async (settings) => {
     const imported = await importStyles()
 
+    console.log(imported)
+
     const selection = figma.currentPage.selection
     const colorIndex = new ColorIndex(imported)
 
-    // console.log('Imported styles ...')
-    // console.log(imported)
-
-    // console.log('Processed colors ...')
-    // colorIndex.processed()
-
-    // const red = {
-    //   b: 0.16333329677581787,
-    //   g: 0.16333329677581787,
-    //   r: 0.8166666626930237,
-    // }
-
-    // const darkRed = {
-    //   b: 0.06444445252418518,
-    //   g: 0.06444445252418518,
-    //   r: 0.4833333194255829
-    // }
-
-    // const gray = {
-    //   b: 0.7686274647712708,
-    //   g: 0.7686274647712708,
-    //   r: 0.7686274647712708
-    // }
-
-    // console.log(colorIndex.compare(red,darkRed))
-    // console.log(colorIndex.compare(red,gray))
-
-    console.log('Linting ...')
+    // console.log('Linting ...')
 
     if (selection?.length === 0) {
       figma.notify('You need to select something to lint 👅')
