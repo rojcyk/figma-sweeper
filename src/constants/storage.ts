c7d274eda56b571f49dee358f7f1828246373de7
Denis Rojcyk <denis@rojcyk.com>
2020-09-05 11:20:40 +0200

    Adding settings to storage

--- src/constants/storage.ts
+++ src/constants/storage.ts
@@ -1,3 +1,4 @@
 export const DOCUMENT_NAME = 'syncedDocumentName'
 export const DOCUMENT_PAINT_STYLES = 'syncedDocumentPaintStyles'
 export const DOCUMENT_ID = 'syncedDocumentId'
+export const SETTINGS = 'settings'
