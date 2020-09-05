d8b2270452960bacbaa5bb48b627bdf849965bf9
Denis Rojcyk <denis@rojcyk.com>
2020-09-05 11:21:32 +0200

    Adding color to typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,16 @@
 declare namespace Plugin {
+
     type appName = 'Appname'
+
+    interface Color {
+      r: number,
+      g: number,
+      b: number,
+      opacity: number
+    }
+
+    interface Style {
+    }
+
   }
   
