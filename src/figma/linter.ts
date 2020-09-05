07a1544c544be0ec1c5536fe31f83f20f8b1e058
Denis Rojcyk <denis@rojcyk.com>
2020-09-05 11:23:11 +0200

    Linting

--- src/figma/linter.ts
+++ src/figma/linter.ts
@@ -7,17 +7,12 @@ import { handleNode } from "./nodeHandler"
   
 export const linter = async () => {
   io.on(APP_LINT, async (settings) => {
-    console.log(settings)
-
     const imported = await importStyles()
-
     const selection = figma.currentPage.selection
     const colorIndex = new ColorIndex(imported, settings)
 
-    // console.log('Linting ...')
-
     if (selection?.length === 0) {
-      figma.notify('You need to select something to lint 👅')
+      figma.notify('You need to select something for the linter to work')
     } else {
       asyncForEach(selection as any[], (frame: any) => {
         handleNode(frame, settings, colorIndex)
