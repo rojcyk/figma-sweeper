277b7cfd8417b77aedde747366c949f6174c384e
Denis Rojcyk <denis@rojcyk.com>
2020-09-06 18:54:34 +0200

    Adding headline component

---
+++ src/app/headline.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import styled from "styled-components"
+
+import { BLACK } from "../constants/ui"
+
+// ******************************** //
+// Interface
+// ******************************** //
+
+export interface HeadlineProps {
+  children: any
+}
+
+// ******************************** //
+// Styles
+// ******************************** //
+
+const HeadlineStyle = styled.span`
+  display: block;
+  font-size: 15px;
+  font-weight: 800;
+  color: ${BLACK};
+  margin: 0;
+  padding: 0;
+`
+
+// ******************************** //
+// Section Class
+// ******************************** //
+
+export const Headline = ({ children }: HeadlineProps) => {
+  return <HeadlineStyle>{children}</HeadlineStyle>
+}
