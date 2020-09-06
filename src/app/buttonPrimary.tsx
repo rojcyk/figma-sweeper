f3e1da26e8f73c2e1226ca146d82cc2e19e5cb7b
Denis Rojcyk <denis@rojcyk.com>
2020-09-06 22:59:50 +0200

    Button now accetps icon, and styles it accordingly

--- src/app/buttonPrimary.tsx
+++ src/app/buttonPrimary.tsx
@@ -26,10 +26,11 @@ const ButtonWrapper = styled.button`
   border-radius: 6px;
   padding: 8px 12px;
   font-size: 14px;
-  font-weight: 600;
+  font-weight: 500;
   text-shadow: 0 1px 0 rgba(0, 0, 0, 0.16);
   box-shadow: inset 0 1px 0 #56a4ff, 0 4px 8px rgba(0, 0, 0, 0.16);
   color: ${WHITE};
+  fill: ${WHITE} !important;
   transition: all 0.2s ease-out;
   outline: none;
 
@@ -55,6 +56,26 @@ const ButtonWrapper = styled.button`
 // Section Class
 // ******************************** //
 
-export const ButtonPrimary = ({ children }: { children: any }) => {
-  return <ButtonWrapper>{children}</ButtonWrapper>
+const Layout = styled.div<{
+  icon?: any
+}>`
+  ${({ icon }) =>
+    icon
+      ? `
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+     `
+      : ""}
+`
+
+export const ButtonPrimary = ({ label, icon }: { label: string; icon?: any }) => {
+  return (
+    <ButtonWrapper>
+      <Layout icon={icon}>
+        {label}
+        {icon}
+      </Layout>
+    </ButtonWrapper>
+  )
 }
