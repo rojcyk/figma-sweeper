55d0cceeaeeedf09d7adf944ab0fd9dc1e8833dc
Denis Rojcyk <denis@rojcyk.com>
2020-09-06 15:02:59 +0200

    Using the new types for exportStyles function

--- src/figma/exportStyles.ts
+++ src/figma/exportStyles.ts
@@ -3,16 +3,9 @@ import io from "figmaio/code"
 import { DOCUMENT_NAME, DOCUMENT_PAINT_STYLES } from "../constants/storage"
 import { STYLES_EXPORT } from "../constants/events"
 
-type ParsableStyle = [
-  boolean,
-  number,
-  {
-    paint: boolean,
-    count: boolean
-  }
-]
-
-export const checkIfStyleParsable = (paints: readonly Paint[] | PaintStyle[]): ParsableStyle => {
+export const checkIfStyleParsable = (
+  paints: readonly Paint[] | PaintStyle[]
+): Plugin.ParsableStyle => {
   let visibleLayers = 0
   let paintRequirement = true
   let position = -1
@@ -23,12 +16,12 @@ export const checkIfStyleParsable = (paints: readonly Paint[] | PaintStyle[]): P
     if (paintStyle.visible) {
       visibleLayers++
 
-      if (paintStyle.type !== 'SOLID') {
+      if (paintStyle.type !== "SOLID") {
         paintRequirement = false
       } else {
         validPosition = position
       }
-    } 
+    }
   })
 
   const lengthRequirement = visibleLayers < 2
@@ -46,34 +39,35 @@ export const checkIfStyleParsable = (paints: readonly Paint[] | PaintStyle[]): P
 
 export const exportStyles = async () => {
   io.on(STYLES_EXPORT, async () => {
-    console.log('Exporting data')
+    console.log("Exporting data")
 
     const localStyles = figma.getLocalPaintStyles()
     const name = figma.root.name
 
-    let paintStyles: object[] = []
+    let paintStyles: Plugin.ExportedStyle[] = []
 
     localStyles.forEach((style) => {
       const [valid, _position, requirement] = checkIfStyleParsable(style.paints)
       const errors = []
 
-      if (!requirement.count) errors.push('The paint style has too many fills.')
-      if (!requirement.paint) errors.push(`The paint style is not a solid fill but a "${style.paints[0].type}"`)
+      if (!requirement.count) errors.push("The paint style has too many fills.")
+      if (!requirement.paint)
+        errors.push(`The paint style is not a solid fill but a "${style.paints[0].type}"`)
 
       paintStyles.push({
         key: style.key,
         name: style.name,
         paint: valid ? style.paints[0] : null,
-        errors: valid ? null : errors,
+        errors: valid ? null : errors
       })
     })
 
     await figma.clientStorage.setAsync(DOCUMENT_NAME, name)
     await figma.clientStorage.setAsync(DOCUMENT_PAINT_STYLES, paintStyles)
 
-    io.send('exported', {
+    io.send("exported", {
       name,
-      paintStyles,
+      paintStyles
     })
   })
 }
