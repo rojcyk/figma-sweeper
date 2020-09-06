1286eab1bc60731cd2e5ee2a4790223bc75efb3f
Denis Rojcyk <denis@rojcyk.com>
2020-09-06 22:18:09 +0200

    Implementing custom headline animation

--- src/app/themeOverview/index.tsx
+++ src/app/themeOverview/index.tsx
@@ -8,10 +8,30 @@ import { SectionWrapper } from "../section"
 // ******************** //
 
 import { generateColorTable } from "./generateColorTable"
-import { Headline } from "../headline"
+import { Headline, HeadlineStyle } from "../headline"
 import { Description } from "../description"
 import { Arrow } from "../arrow"
 import { ButtonPrimary } from "../buttonPrimary"
+
+const HeadlineAnimated = styled(HeadlineStyle)<{
+  expanded: boolean
+}>`
+  transition: all 0.2s ease-out;
+  position: absolute;
+  top: ${({ expanded }) => (expanded ? "18px" : "0")};
+  left: 30px;
+  opacity: ${({ expanded }) => (expanded ? "1" : "0")};
+`
+
+const LoadedStyle = styled.div<{
+  expanded: boolean
+}>`
+  transition: all 0.2s ease-out;
+  position: absolute;
+  top: ${({ expanded }) => (expanded ? "24px" : "10px")};
+  left: 30px;
+  opacity: ${({ expanded }) => (expanded ? "0" : "1")};
+`
 
 const ContentWrapper = styled.div`
   padding: 0 16px 16px 16px;
@@ -19,27 +39,54 @@ const ContentWrapper = styled.div`
 
 const ButtonWrapper = styled.div`
   display: flex;
-  padding: 16px;
   align-items: center;
+  padding-left: 16px;
+  // padding: 16px;
+  height: 56px;
+  position: relative;
 `
 
-const Header = ({ expanded }: { expanded: boolean }) => {
+const Header = ({
+  expanded,
+  name,
+  paintStyles,
+  hasStyles
+}: {
+  expanded: boolean
+  name: string
+  paintStyles: number
+  hasStyles: boolean
+}) => {
+  // const margin = expanded ? "10px" : "0px"
   return (
     <ButtonWrapper>
-      <Arrow expanded={expanded} />
-      <Headline>Colors</Headline>
+      <Arrow expanded={expanded} style={{ opacity: hasStyles ? "1" : "0.5" }} />
+      <HeadlineAnimated expanded={expanded}>Sync styles</HeadlineAnimated>
+      <LoadedStyle expanded={expanded}>
+        <Headline>{name}</Headline>
+        <Description>{paintStyles} paint styles</Description>
+      </LoadedStyle>
     </ButtonWrapper>
   )
 }
 
-const Content = () => {
+const Content = ({
+  name,
+  paintStyles,
+  hasStyles
+}: {
+  name: string
+  paintStyles: number
+  hasStyles: boolean
+}) => {
   return (
     <ContentWrapper>
       <Description style={{ marginBottom: "12px", marginTop: "4px" }}>
         The plugin will export the styles from the file, and reference them later on when linting.
       </Description>
 
-      <ButtonPrimary>Sync document</ButtonPrimary>
+      {hasStyles === false && <ButtonPrimary>Upload this document</ButtonPrimary>}
+      {hasStyles === true && <ButtonPrimary>Unsync document</ButtonPrimary>}
     </ContentWrapper>
   )
 }
@@ -47,6 +94,9 @@ const Content = () => {
 export const ThemeOverview = ({ styles, name }: Plugin.ThemeOverviewI) => {
   const paintStyles = generateColorTable(styles.paintStyles)
   const [expanded, setExpanded] = React.useState(false)
+  const hasStyles = styles.paintStyles.length > 0 ? true : false
+
+  if (hasStyles === false) setExpanded(true)
 
   const toggleHandler = () => {
     if (expanded) {
@@ -57,94 +107,20 @@ export const ThemeOverview = ({ styles, name }: Plugin.ThemeOverviewI) => {
   }
 
   return (
-    <React.Fragment>
-      <SectionWrapper
-        expanded={expanded}
-        content={Content}
-        button={Header}
-        buttonHandler={toggleHandler}
-      />
-      {/* <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginTop: "16px",
-          marginBottom: "16px"
-        }}
-      >
-        <h2
-          style={{
-            display: "inline-block",
-            margin: "0",
-            padding: "0"
-          }}
-        >
-          Sync
-        </h2>
-
-        <button
-          onClick={(e: any) => {
-            io.send(STYLES_EXPORT)
-          }}
-        >
-          Current doc
-        </button>
-      </div>
-
-      <p
-        style={{
-          lineHeight: "18px",
-          opacity: "0.8"
-        }}
-      >
-        The plugin will look into this file and search for your paint styles. The file must be{" "}
-        <a
-          href="https://help.figma.com/hc/en-us/articles/360039162653-Publish-a-file-to-a-Team-Library"
-          target="_blank"
-        >
-          published
-        </a>
-        .
-      </p>
-
-      <div style={{ marginBottom: "4px" }}>
-        <b>
-          <strong style={{ fontSize: "9px", opacity: "0.3" }}>►</strong> Synced document:
-        </b>{" "}
-        {name && <span style={{ marginRight: "4px" }}>{name}</span>}
-      </div>
-      <details
-        style={{
-          marginBottom: "12px"
-        }}
-      >
-        <summary
-          style={{
-            marginBottom: "8px",
-            fontSize: "12px",
-            outline: "none",
-            cursor: "pointer"
-          }}
-        >
-          <b>Synced colors:</b> {styles.paintStyles ? styles.paintStyles.length : ""}
-        </summary>
-
-        <p
-          style={{
-            fontSize: "12px",
-            lineHeight: "18px",
-            opacity: "0.5",
-            padding: "8px",
-            border: "1px solid rgba(0,0,0,0.14)",
-            borderRadius: "3px"
-          }}
-        >
-          For now, the plugin works with color styles that have a <b>single and solid</b> fill.
-        </p>
-
-        {paintStyles}
-      </details> */}
-    </React.Fragment>
+    <SectionWrapper
+      expanded={expanded}
+      content={
+        <Content name={name} paintStyles={styles.paintStyles.length} hasStyles={hasStyles} />
+      }
+      button={
+        <Header
+          expanded={expanded}
+          name={name}
+          paintStyles={styles.paintStyles.length}
+          hasStyles={hasStyles}
+        />
+      }
+      buttonHandler={toggleHandler}
+    />
   )
 }
