90a0de7f6c2b5063a8ed212f5d53729b496c5f85
Denis Rojcyk <denis@rojcyk.com>
2020-09-06 18:49:40 +0200

    Adding a couple of basic colors

--- src/constants/ui.ts
+++ src/constants/ui.ts
@@ -1,3 +1,9 @@
 export const WINDOW_WIDTH = 360
 export const WINDOW_HEIGHT = 560
 export const ANIMATION_SPEED = "300ms"
+
+export const WHITE = "#FFFFFF"
+export const BLACK = "#26303B"
+export const GRAY = "#5C6975"
+export const SEPARATOR = "#DFE6EF"
+export const BACKGROUND = "#F0F2F5"
