e1869a6ee77c7b3e7088d0029d81746a623b3751
Denis Rojcyk <denis@rojcyk.com>
2020-09-07 21:50:43 +0200

    Creating new delete styles listener

---
+++ src/figma/deleteStyles.tsx
@@ -0,0 +1,18 @@
+import io from "figmaio/code"
+
+import { DOCUMENT_NAME, DOCUMENT_PAINT_STYLES } from "../constants/storage"
+import { STYLES_DELETE, STYLES_UPDATE } from "../constants/events"
+
+export const deleteStyles = async () => {
+  io.on(STYLES_DELETE, async () => {
+    console.log("Deleting data")
+
+    await figma.clientStorage.setAsync(DOCUMENT_NAME, "")
+    await figma.clientStorage.setAsync(DOCUMENT_PAINT_STYLES, [])
+
+    io.send(STYLES_UPDATE, {
+      name: "",
+      paintStyles: []
+    })
+  })
+}
