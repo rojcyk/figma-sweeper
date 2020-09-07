168212fc69356c10d89a3a905e8fee609509590d
Denis Rojcyk <denis@rojcyk.com>
2020-09-07 21:51:13 +0200

    Logs for better debuging

--- src/figma/linter.ts
+++ src/figma/linter.ts
@@ -1,18 +1,20 @@
 import io from "figmaio/code"
 import { APP_LINT } from "../constants/events"
-import { ColorIndex } from './colorIndex'
-import { importStyles } from './importStyles'
+import { ColorIndex } from "./colorIndex"
+import { importStyles } from "./importStyles"
 import asyncForEach from "../helpers/asyncForEach"
 import { handleNode } from "./nodeHandler"
-  
+import { getSettings } from "./getSettings"
+
 export const linter = async () => {
   io.on(APP_LINT, async (settings) => {
+    console.log(settings)
     const imported = await importStyles()
     const selection = figma.currentPage.selection
     const colorIndex = new ColorIndex(imported, settings)
 
     if (selection?.length === 0) {
-      figma.notify('You need to select something for the linter to work')
+      figma.notify("You need to select something for the linter to work")
     } else {
       asyncForEach(selection as any[], (frame: any) => {
         handleNode(frame, settings, colorIndex)
