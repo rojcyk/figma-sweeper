74e2f5b0550489d04a536817cafafc97ad6cf837
Denis Rojcyk <denis@rojcyk.com>
2020-09-07 21:50:31 +0200

    Adding new events styles delete and update

--- src/constants/events.ts
+++ src/constants/events.ts
@@ -1,4 +1,6 @@
 export const APP_START = "appStart"
 export const APP_LINT = "appLint"
 export const STYLES_EXPORT = "stylesExport"
+export const STYLES_DELETE = "stylesDelete"
+export const STYLES_UPDATE = "stylesUpdated"
 export const COLOR_SETTINGS_CHANGE = "colorSettingsChange"
