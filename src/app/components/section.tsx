ea7af457601375cd1ca61552ce1c108b94ffe7c6
Denis Rojcyk <denis@rojcyk.com>
2020-09-10 22:14:18 +0200

    Section now has a different styling when it is not active

--- src/app/components/section.tsx
+++ src/app/components/section.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import AnimateHeight from "react-animate-height"
 
-import { WHITE, SEPARATOR } from "../../constants/ui"
+import { WHITE, SEPARATOR, BACKGROUND } from "../../constants/ui"
 
 // ******************************** //
 // Interface
@@ -13,6 +13,7 @@ export interface SectionProps {
   buttonHandler: Function
   content: any
   button: any
+  isSynced?: boolean
 }
 
 interface SectionState {
@@ -33,6 +34,7 @@ const ButtonWrapper = styled.div`
 
 const Wrapper = styled.div<{
   expanded: boolean
+  active: boolean
 }>`
   transition: all 0.3s ease-out;
   background-color: ${WHITE};
@@ -41,19 +43,39 @@ const Wrapper = styled.div<{
   border-top: ${(props) => (props.expanded ? `3px solid ${SEPARATOR}` : `0 solid ${SEPARATOR}`)};
   border-bottom: ${(props) =>
     props.expanded ? `4px solid ${SEPARATOR}` : `1px solid ${SEPARATOR}`};
+
+  ${({ active }) =>
+    active
+      ? `
+    `
+      : `
+      pointer-events: none;
+      background-color: ${BACKGROUND};
+      `}
 `
 
 // ******************************** //
 // Section Class
 // ******************************** //
 
-export const SectionWrapper = ({ content, button, buttonHandler, expanded }: SectionProps) => {
+export const SectionWrapper = ({
+  content,
+  button,
+  buttonHandler,
+  expanded,
+  isSynced
+}: SectionProps) => {
   // const Content = content
   // const Button = button
   const height = expanded ? "auto" : 0
+  const active = isSynced === undefined ? false : isSynced
+
+  console.log("------------------")
+  console.log(isSynced)
+  console.log(active)
 
   return (
-    <Wrapper expanded={expanded}>
+    <Wrapper expanded={expanded} active={active}>
       <ButtonWrapper onClick={() => buttonHandler()}>{button}</ButtonWrapper>
 
       <AnimateHeight duration={220} height={height}>
