a795f28be672b4ca055d8b89e5ca048c71f1d23d
Denis Rojcyk <denis@rojcyk.com>
2020-09-12 12:29:02 +0200

    Removing the old code (from the previously exported components)

--- src/app/colors/index.tsx
+++ src/app/colors/index.tsx
@@ -3,12 +3,10 @@ import React from "react"
 import styled from "styled-components"
 
 import { SectionWrapper } from "../components/section"
-import { Headline } from "../components/headline"
-import { Arrow } from "../icons/arrow"
 import { Description } from "../components/description"
-import { Check } from "../components/check"
+import Checkbox from "../components/checkbox"
 import { COLOR_SETTINGS_CHANGE } from "../../constants/events"
-import { BACKGROUND, WHITE } from "../../constants/ui"
+import SectionHeader from "../components/sectionHeader"
 
 interface ColorSettingsProp {
   settings: Plugin.ColorSettings
@@ -21,86 +19,14 @@ interface ColorSettingsState {
   settings: Plugin.ColorSettings
 }
 
-const HeaderWrapper = styled.div<{ isSynced: boolean }>`
-  display: flex;
-  align-items: center;
-  padding: 16px 16px;
-
-  ${({ isSynced }) =>
-    isSynced
-      ? `
-      background-color: ${WHITE};
-    `
-      : `
-      svg,
-      span {
-        color: #8593A3 !important;
-      }
-
-      box-shadow: inset 0 1px 0 ${WHITE};
-      background-color: ${BACKGROUND};
-    `}
-`
-
 const ContentWrapper = styled.div`
   padding: 0 16px;
 `
 
-const CheckboxWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 4px;
-  padding: 4px;
-  border-radius: 4px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #f2f4f7;
-  }
-`
-
-const CheckboxLabel = styled.span<{
-  checked: boolean
-}>`
-  font-size: 13px;
-  color: ${({ checked }) => (checked ? "#000000" : "#6F737D")};
-  font-weight: 500;
-`
-
-const Checkbox = ({
-  checked,
-  children,
-  onCheckboxChange
-}: {
-  checked: boolean
-  children: any
-  onCheckboxChange: Function
-}) => {
-  return (
-    <CheckboxWrapper onClick={() => onCheckboxChange()}>
-      <Check checked={checked} />
-      <CheckboxLabel checked={checked} style={{ display: "inline-block" }}>
-        {children}
-      </CheckboxLabel>
-    </CheckboxWrapper>
-  )
-}
-
-const Header = ({ expanded, isSynced }: { expanded: boolean; isSynced: boolean }) => {
-  return (
-    <HeaderWrapper isSynced={isSynced}>
-      <Arrow direction={expanded ? "down" : "right"} style={{ opacity: isSynced ? "1" : "0.25" }} />
-      <Headline>Colors</Headline>
-    </HeaderWrapper>
-  )
-}
-
 const Content = ({
-  expanded,
   onSettingsChange,
   settings
 }: {
-  expanded: boolean
   onSettingsChange: Function
   settings: Plugin.ColorSettings
 }) => {
@@ -197,11 +123,16 @@ export class SettingsForm extends React.Component<ColorSettingsProp, ColorSettin
       <SectionWrapper
         isSynced={this.props.isSynced}
         expanded={this.props.isSynced ? this.state.expanded : false}
-        button={<Header expanded={this.state.expanded} isSynced={this.props.isSynced} />}
+        button={
+          <SectionHeader
+            label={"Colors"}
+            isExpanded={this.state.expanded}
+            isActive={this.props.isSynced}
+          />
+        }
         content={
           <Content
             settings={this.state.settings}
-            expanded={this.state.expanded}
             onSettingsChange={this.toggleSettings.bind(this)}
           />
         }
