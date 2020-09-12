b16aa4249ec3bc07a82e9dd37d0b49b5cdfa3029
Denis Rojcyk <denis@rojcyk.com>
2020-09-12 12:28:47 +0200

    Updating the checkbox design

--- src/app/components/checkbox.tsx
+++ src/app/components/checkbox.tsx
@@ -1,20 +1,45 @@
 import React from "react"
+import { Check } from "../components/check"
+import styled from "styled-components"
+
+const CheckboxWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 4px;
+  padding: 4px;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f2f4f7;
+  }
+`
+
+const CheckboxLabel = styled.span<{
+  checked: boolean
+}>`
+  font-size: 13px;
+  color: ${({ checked }) => (checked ? "#000000" : "#6F737D")};
+  font-weight: 500;
+`
 
 const Checkbox = ({
-  label,
-  isSelected,
+  checked,
+  children,
   onCheckboxChange
 }: {
-  label: string
-  isSelected: boolean
-  onCheckboxChange: any
-}) => (
-  <div>
-    <label>
-      <input type="checkbox" name={label} checked={isSelected} onChange={onCheckboxChange} />
-      {label}
-    </label>
-  </div>
-)
+  checked: boolean
+  children: any
+  onCheckboxChange: Function
+}) => {
+  return (
+    <CheckboxWrapper onClick={() => onCheckboxChange()}>
+      <Check checked={checked} />
+      <CheckboxLabel checked={checked} style={{ display: "inline-block" }}>
+        {children}
+      </CheckboxLabel>
+    </CheckboxWrapper>
+  )
+}
 
 export default Checkbox
