073264fffc7f4a85cdf210197e19a53094cd6199
Denis Rojcyk <denis@rojcyk.com>
2020-09-12 14:08:12 +0200

    Using the updated typings and proper settings

--- src/figma/checkFill.ts
+++ src/figma/checkFill.ts
@@ -1,26 +1,23 @@
-import { ColorIndex } from './colorIndex'
-import { checkIfStyleParsable } from './exportStyles'
+import { ColorIndex } from "./colorIndex"
+import { checkIfStyleParsable } from "./exportStyles"
 
-export const checkFills = (node: any, colorIndex: ColorIndex, settings: any) => {
-  // When there is no fill, we have nothing to do  
+export const checkFills = (node: any, colorIndex: ColorIndex, settings: Plugin.Settings) => {
+  // When there is no fill, we have nothing to do
   if (node.fills.length === 0) return null
 
   // If the fill style is already set ignore it, unless specified otherwise by settings
-  if (node.fillStyleId !== "" && !settings.overwriteStyle) return null
+  if (node.fillStyleId !== "" && !settings.color.overwriteStyles) return null
 
   // Check if the style is single, solid color and we can change it, otherwise, ignore it.
-  const [
-    isValid,
-    position
-  ] = checkIfStyleParsable(node.fills)
+  const [isValid, position] = checkIfStyleParsable(node.fills)
 
   // If the style is not parsable, return null
   if (isValid !== true) return null
 
   // We contstruct the color for easier processing
-  const color = {
+  const color: RGBA = {
     ...node.fills[position].color,
-    opacity: node.fills[position].opacity
+    a: node.fills[position].opacity
   }
 
   // First, before we even start comparing, we check whether the color already exists
@@ -30,7 +27,7 @@ export const checkFills = (node: any, colorIndex: ColorIndex, settings: any) => 
     node.fillStyleId = foundColor.id
     return foundColor.id
   } else {
-    if (settings.findClosest) {
+    if (settings.color.findClosestColor) {
       const comparedColor = colorIndex.findSimilarColor(color)
       node.fillStyleId = comparedColor.id
       return comparedColor.id
--- src/figma/checkStroke.ts
+++ src/figma/checkStroke.ts
@@ -1,35 +1,32 @@
-import { ColorIndex } from './colorIndex'
-import { checkIfStyleParsable } from './exportStyles'
+import { ColorIndex } from "./colorIndex"
+import { checkIfStyleParsable } from "./exportStyles"
 
-export const checkStrokes = (node: any, colorIndex: ColorIndex, settings: any) => {
-  // When there is no fill, we have nothing to do  
+export const checkStrokes = (node: any, colorIndex: ColorIndex, settings: Plugin.Settings) => {
+  // When there is no fill, we have nothing to do
   if (node.strokes.length === 0) return null
 
   // If the fill style is already set ignore it, unless specified otherwise by settings
-  if (node.strokeStyleId !== "" && !settings.overwriteStyle) return null
+  if (node.fillStyleId !== "" && !settings.color.overwriteStyles) return null
 
   // Check if the style is single, solid color and we can change it, otherwise, ignore it.
-  const [
-    isValid,
-    position
-  ] = checkIfStyleParsable(node.strokes)
+  const [isValid, position] = checkIfStyleParsable(node.strokes)
 
   // If the style is not parsable, return null
   if (isValid !== true) return null
-  
+
   // We contstruct the color for easier processing
-  const color = {
+  const color: RGBA = {
     ...node.strokes[position].color,
-    opacity: node.strokes[position].opacity
+    a: node.strokes[position].opacity
   }
-  
+
   const foundColor = colorIndex.findImportedColor(color)
 
   if (foundColor) {
     node.strokeStyleId = foundColor.id
     return foundColor.id
   } else {
-    if (settings.findClosest) {
+    if (settings.color.findClosestColor) {
       const comparedColor = colorIndex.findSimilarColor(color)
       node.strokeStyleId = comparedColor.id
       return comparedColor.id
