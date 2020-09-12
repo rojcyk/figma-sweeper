fd5a165a310b71b6e6dd6cd52e3eed6896c75424
Denis Rojcyk <denis@rojcyk.com>
2020-09-12 14:06:45 +0200

    We are getting the settings saved in figma instead of passing them from the plugin

--- src/figma/linter.ts
+++ src/figma/linter.ts
@@ -7,8 +7,8 @@ import { handleNode } from "./nodeHandler"
 import { getSettings } from "./getSettings"
 
 export const linter = async () => {
-  io.on(APP_LINT, async (settings) => {
-    console.log(settings)
+  io.on(APP_LINT, async () => {
+    const settings = await getSettings()
     const imported = await importStyles()
     const selection = figma.currentPage.selection
     const colorIndex = new ColorIndex(imported, settings)
