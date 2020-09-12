cc7d4f45ba28e24a716deb52766e2c342bc15d4b
Denis Rojcyk <denis@rojcyk.com>
2020-09-12 12:28:38 +0200

    Exporting section header into its own component

---
+++ src/app/components/sectionHeader.tsx
@@ -0,0 +1,47 @@
+import React from "react"
+import styled from "styled-components"
+
+import { Headline } from "../components/headline"
+import { Arrow } from "../icons/arrow"
+import { BACKGROUND, WHITE } from "../../constants/ui"
+
+const HeaderWrapper = styled.div<{ isSynced: boolean }>`
+  display: flex;
+  align-items: center;
+  padding: 16px 16px;
+
+  ${({ isSynced }) =>
+    isSynced
+      ? `
+      background-color: ${WHITE};
+    `
+      : `
+      svg,
+      span {
+        color: #8593A3 !important;
+      }
+
+      box-shadow: inset 0 1px 0 ${WHITE};
+      background-color: ${BACKGROUND};
+    `}
+`
+
+export default ({
+  isExpanded,
+  isActive,
+  label
+}: {
+  isExpanded: boolean
+  isActive: boolean
+  label: string
+}) => {
+  return (
+    <HeaderWrapper isSynced={isActive}>
+      <Arrow
+        direction={isExpanded ? "down" : "right"}
+        style={{ opacity: isActive ? "1" : "0.25" }}
+      />
+      <Headline>{label}</Headline>
+    </HeaderWrapper>
+  )
+}
