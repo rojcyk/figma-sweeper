3216e774ad1004aeb2e2b3e10e3f95547f24edc6
Denis Rojcyk <denis@rojcyk.com>
2020-09-12 14:06:58 +0200

    nodeHandler is now using proper settings

--- src/figma/nodeHandler.ts
+++ src/figma/nodeHandler.ts
@@ -1,94 +1,95 @@
-import { checkFills } from './checkFill'
-import { checkStrokes } from './checkStroke'
-import { traverse } from './traverse'
+import { checkFills } from "./checkFill"
+import { checkStrokes } from "./checkStroke"
+import { traverse } from "./traverse"
+import { ColorIndex } from "./colorIndex"
 
 // ************************* */
 /* NodeHandling
 // ************************* */
 
-export const handleNode = (node: any, settings: any, colorIndex: any) => {
-  const { fills, strokes } = settings
+export const handleNode = (node: any, settings: Plugin.Settings, colorIndex: ColorIndex) => {
+  const { overwriteFills, overwriteStrokes } = settings.color
 
   switch (node.type) {
-    case 'FRAME':
+    case "FRAME":
       traverse(node.children, handleNode, settings, colorIndex)
 
-      if (fills) checkFills(node, colorIndex, settings)
-      if (strokes) checkStrokes(node, colorIndex, settings)
+      if (overwriteFills) checkFills(node, colorIndex, settings)
+      if (overwriteStrokes) checkStrokes(node, colorIndex, settings)
       break
 
-    case 'GROUP':
+    case "GROUP":
       traverse(node.children, handleNode, settings, colorIndex)
 
-      if (fills) checkFills(node, colorIndex, settings)
-      if (strokes) checkStrokes(node, colorIndex, settings)
+      if (overwriteFills) checkFills(node, colorIndex, settings)
+      if (overwriteStrokes) checkStrokes(node, colorIndex, settings)
       break
 
-    case 'COMPONENT':
+    case "COMPONENT":
       traverse(node.children, handleNode, settings, colorIndex)
 
-      if (fills) checkFills(node, colorIndex, settings)
-      if (strokes) checkStrokes(node, colorIndex, settings)
+      if (overwriteFills) checkFills(node, colorIndex, settings)
+      if (overwriteStrokes) checkStrokes(node, colorIndex, settings)
       break
 
-    case 'INSTANCE':
+    case "INSTANCE":
       traverse(node.children, handleNode, settings, colorIndex)
 
-      if (fills) checkFills(node, colorIndex, settings)
-      if (strokes) checkStrokes(node, colorIndex, settings)
+      if (overwriteFills) checkFills(node, colorIndex, settings)
+      if (overwriteStrokes) checkStrokes(node, colorIndex, settings)
       break
     //* *************************/
     /* items below do not have children 
     //* *************************/
-    case 'BOOLEAN_OPERATION':
+    case "BOOLEAN_OPERATION":
       // console.log(`this type has children but we shouldn't work with them`)
-      if (fills) checkFills(node, colorIndex, settings)
-      if (strokes) checkStrokes(node, colorIndex, settings)
+      if (overwriteFills) checkFills(node, colorIndex, settings)
+      if (overwriteStrokes) checkStrokes(node, colorIndex, settings)
       break
 
-    case 'VECTOR':
+    case "VECTOR":
       // console.log('this type has NOT children')
-      if (fills) checkFills(node, colorIndex, settings)
-      if (strokes) checkStrokes(node, colorIndex, settings)
+      if (overwriteFills) checkFills(node, colorIndex, settings)
+      if (overwriteStrokes) checkStrokes(node, colorIndex, settings)
       break
 
-    case 'STAR':
+    case "STAR":
       // console.log('this type has NOT children')
-      if (fills) checkFills(node, colorIndex, settings)
-      if (strokes) checkStrokes(node, colorIndex, settings)
+      if (overwriteFills) checkFills(node, colorIndex, settings)
+      if (overwriteStrokes) checkStrokes(node, colorIndex, settings)
       break
 
-    case 'LINE':
+    case "LINE":
       // console.log('this type has NOT children')
-      if (fills) checkFills(node, colorIndex, settings)
-      if (strokes) checkStrokes(node, colorIndex, settings)
+      if (overwriteFills) checkFills(node, colorIndex, settings)
+      if (overwriteStrokes) checkStrokes(node, colorIndex, settings)
       break
 
-    case 'ELLIPSE':
+    case "ELLIPSE":
       // console.log('this type has NOT children')
-      if (fills) checkFills(node, colorIndex, settings)
-      if (strokes) checkStrokes(node, colorIndex, settings)
+      if (overwriteFills) checkFills(node, colorIndex, settings)
+      if (overwriteStrokes) checkStrokes(node, colorIndex, settings)
       break
 
-    case 'POLYGON':
+    case "POLYGON":
       // console.log('this type has NOT children')
-      if (fills) checkFills(node, colorIndex, settings)
-      if (strokes) checkStrokes(node, colorIndex, settings)
+      if (overwriteFills) checkFills(node, colorIndex, settings)
+      if (overwriteStrokes) checkStrokes(node, colorIndex, settings)
       break
 
-    case 'RECTANGLE':
-      if (fills) checkFills(node, colorIndex, settings)
-      if (strokes) checkStrokes(node, colorIndex, settings)
-      // console.log('this type has NOT children')
-      break
-
-    case 'TEXT':
-      if (fills) checkFills(node, colorIndex, settings)
-      if (strokes) checkStrokes(node, colorIndex, settings)
+    case "RECTANGLE":
+      if (overwriteFills) checkFills(node, colorIndex, settings)
+      if (overwriteStrokes) checkStrokes(node, colorIndex, settings)
       // console.log('this type has NOT children')
       break
 
-    case 'SLICE':
+    case "TEXT":
+      if (overwriteFills) checkFills(node, colorIndex, settings)
+      if (overwriteStrokes) checkStrokes(node, colorIndex, settings)
+      // console.log('this type has NOT children')
+      break
+
+    case "SLICE":
       // This node type is not valid for anything
       break
   }
