a43fa3b0871089eeb613b4a65645ddc318a35e14
Denis Rojcyk <denis@rojcyk.com>
2020-09-13 14:48:12 +0200

    Fixed animatin for header

--- src/app/themeOverview/themeOverviewHeader.tsx
+++ src/app/themeOverview/themeOverviewHeader.tsx
@@ -8,18 +8,8 @@ import styled from "styled-components"
 import { Headline, HeadlineStyle } from "../components/headline"
 import { Description } from "../components/description"
 import { Arrow } from "../icons/arrow"
-import { ANIMATION_SPEED_MS, BACKGROUND } from "../../constants/ui"
-
-const HeadlineAnimated = styled(HeadlineStyle)<{
-  expanded: boolean
-}>`
-  user-select: none;
-  transition: all ${ANIMATION_SPEED_MS}ms ease-out;
-  position: absolute;
-  top: ${({ expanded }) => (expanded ? "19px" : "0")};
-  left: 32px;
-  opacity: ${({ expanded }) => (expanded ? "1" : "0")};
-`
+import { ANIMATION_SPEED_MS, BACKGROUND, WHITE } from "../../constants/ui"
+import { HeaderBackground } from "../components/sectionHeader"
 
 const ButtonWrapper = styled.div<{
   isSynced: boolean
@@ -29,24 +19,42 @@ const ButtonWrapper = styled.div<{
   padding-left: 16px;
   height: 56px;
   position: relative;
-  transition: all ${ANIMATION_SPEED_MS}ms ease-out;
 
   ${({ isSynced }) =>
     isSynced
       ? `
-        &:hover {
-          background-color: ${BACKGROUND};
-          transition: all ${ANIMATION_SPEED_MS}ms ease-out;
-        }
-  `
+    cursor: pointer;
+
+    .background {
+      background-color: ${WHITE};
+    }
+
+    &:hover {
+      .background {
+        background-color: ${BACKGROUND};
+      }
+    }
+    `
       : `
-  `}
+    cursor: default;
+`}
+`
+
+const HeadlineAnimated = styled(HeadlineStyle)<{
+  expanded: boolean
+}>`
+  z-index: 10;
+  transition: all ${ANIMATION_SPEED_MS}ms ease-out;
+  position: absolute;
+  top: ${({ expanded }) => (expanded ? "19px" : "0")};
+  left: 32px;
+  opacity: ${({ expanded }) => (expanded ? "1" : "0")};
 `
 
 const LoadedStyle = styled.div<{
   expanded: boolean
 }>`
-  user-select: none;
+  z-index: 10;
   transition: all ${ANIMATION_SPEED_MS}ms ease-out;
   position: absolute;
   top: ${({ expanded }) => (expanded ? "24px" : "10px")};
@@ -68,16 +76,10 @@ export default ({
   showArrow: boolean
 }) => {
   return (
-    <ButtonWrapper
-      isSynced={isSynced}
-      style={{
-        cursor: isSynced ? "pointer" : "default",
-        pointerEvents: isSynced ? "auto" : "none"
-      }}
-    >
+    <ButtonWrapper isSynced={isSynced}>
       <Arrow
         direction={expanded ? "down" : "right"}
-        style={{ opacity: showArrow ? "1" : "0.25" }}
+        style={{ opacity: showArrow ? "1" : "0.25", zIndex: 10 }}
       />
 
       {name === "" ? (
@@ -91,6 +93,8 @@ export default ({
           </LoadedStyle>
         </React.Fragment>
       )}
+
+      <HeaderBackground className={"background"} isExpanded={expanded} />
     </ButtonWrapper>
   )
 }
