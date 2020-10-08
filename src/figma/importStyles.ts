83be2d476473ca1fb52821589089a6f9e7290891
Denis Rojcyk <denis@rojcyk.com>
2020-10-08 20:48:44 +0200

    Add error when linting styles from different file

--- src/figma/importStyles.ts
+++ src/figma/importStyles.ts
@@ -2,26 +2,30 @@ import io from "figmaio/code"
 import { DOCUMENT_PAINT_STYLES } from "../constants/storage"
 import asyncForEach from "../helpers/asyncForEach"
 
-export const isImported = (arr: any[], key : string) => {
-  return arr.find((el) => { 
+export const isImported = (arr: any[], key: string) => {
+  return arr.find((el) => {
     if (el.key === key) return el
     else return undefined
   })
 }
-  
+
 export const importStyles = async () => {
-  let importedStyles: any[] = []
+  try {
+    let importedStyles: any[] = []
 
-  const localStyles = figma.getLocalPaintStyles()
-  const styles = await figma.clientStorage.getAsync(DOCUMENT_PAINT_STYLES)
+    const localStyles = figma.getLocalPaintStyles()
+    const styles = await figma.clientStorage.getAsync(DOCUMENT_PAINT_STYLES)
 
-  await asyncForEach(styles, async (style: any) => {
-    if (style.errors === null) {
-      let imported = isImported(localStyles, style.key)
-      if (imported === undefined) imported = await figma.importStyleByKeyAsync(style.key)
-      importedStyles.push(imported)
-    }
-  })
+    await asyncForEach(styles, async (style: any) => {
+      if (style.errors === null) {
+        let imported = isImported(localStyles, style.key)
+        if (imported === undefined) imported = await figma.importStyleByKeyAsync(style.key)
+        importedStyles.push(imported)
+      }
+    })
 
-  return importedStyles
+    return importedStyles
+  } catch (e) {
+    return undefined
+  }
 }
--- src/figma/listeners/linterListener.ts
+++ src/figma/listeners/linterListener.ts
@@ -8,18 +8,45 @@ import { getSettings } from "../getSettings"
 
 export const linterListener = async () => {
   io.on(APP_LINT, async () => {
+    // Get the currently saved settings.
     const settings = await getSettings()
-    const imported = await importStyles()
-    const selection = figma.currentPage.selection
-    const colorIndex = new ColorIndex(imported, settings)
 
+    // Get the currently saved settings.
+    const imported = await importStyles()
+    if (imported === undefined) {
+      figma.notify("🚧 Synced styles are from another file. You need to publish them first.")
+      return null
+    }
+
+    // If there is nothing selected, we have nothing to do.
+    const selection = figma.currentPage.selection
     if (selection?.length === 0) {
       figma.notify("You need to select something for the linter to work")
-    } else {
-      await asyncForEach(selection as [], (frame: SceneNode) => {
-        handleNode(frame, settings, colorIndex)
-        figma.notify("Selection linted 👊")
-      })
+      return null
     }
+
+    // All prerequisities are met, we can lint!
+    const colorIndex = new ColorIndex(imported, settings)
+    await asyncForEach(selection as [], (frame: SceneNode) => {
+      handleNode(frame, settings, colorIndex)
+    })
+
+    figma.notify("Selection linted 👊")
+
+    // if (imported) {
+    //   // const selection = figma.currentPage.selection
+    //   const colorIndex = new ColorIndex(imported, settings)
+
+    //   if (selection?.length === 0) {
+    //     figma.notify("You need to select something for the linter to work")
+    //   } else {
+    //     await asyncForEach(selection as [], (frame: SceneNode) => {
+    //       handleNode(frame, settings, colorIndex)
+    //     })
+    //     figma.notify("Selection linted 👊")
+    //   }
+    // } else {
+    //   figma.notify("🚧 Synced styles are from another file. You need to publish them first.")
+    // }
   })
 }
